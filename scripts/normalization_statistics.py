6bca4e14837ff011c8520776595ce1aae11d41c1
kpetrakis-eden <kostispetrak@edencore.ai>
2026-07-10 15:57:49 +0300

    norm statistics for blueberries v2

--- scripts/normalization_statistics.py
+++ scripts/normalization_statistics.py
@@ -6,7 +6,7 @@ import torchvision.transforms.v2 as v2
 from src.utils.extra import PadToSquare
 from tqdm.auto import tqdm
 
-def compute_normalization_statistics(data_dir:Path, img_size:int, interpolation:v2.InterpolationMode):
+def compute_normalization_statistics(data_dir:Path, batch_size:int, img_size:int, interpolation:v2.InterpolationMode):
   '''
   Calculate train data mean and std for normalization.
   '''
@@ -18,7 +18,7 @@ def compute_normalization_statistics(data_dir:Path, img_size:int, interpolation:
     v2.ToDtype(torch.float32, scale=True)
   ])
   train_dataset = ImageFolder(data_dir / "train", transform=transforms)
-  train_loader = DataLoader(train_dataset, batch_size=256, shuffle=False, num_workers=12, generator=g)
+  train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=12, generator=g)
 
   channels_sum = torch.zeros(3, dtype=torch.float64) # 0
   channels_squared_sum = torch.zeros(3, dtype=torch.float64) # 0
@@ -38,10 +38,12 @@ def compute_normalization_statistics(data_dir:Path, img_size:int, interpolation:
 # Usage: uv run python -m scripts.normalization_statistics
 if __name__ == "__main__":
   # torch.set_printoptions(precision=4)
-  IMG_SIZE = 96
+  IMG_SIZE = 64 # 96
+  BATCH_SIZE = 2048
   INTERPOLATION = v2.InterpolationMode.BICUBIC
   # data_dir = Path("data/processed/classification-expanded-boxes") # same as in config.yaml
-  data_dir = Path("data/processed/classification")
-  mean, std = compute_normalization_statistics(data_dir, IMG_SIZE, INTERPOLATION)
-  print(f"Mean: {mean} | std: {std}")
+  dataset = "classification-merged-pink-purple-v2"
+  data_dir = Path(f"data/processed/{dataset}")
+  mean, std = compute_normalization_statistics(data_dir, BATCH_SIZE, IMG_SIZE, INTERPOLATION)
+  print(f"({dataset}) Mean: {mean} | std: {std}")
   # print(f"Mean: {mean.tolist()} | std: {std.tolist()}")
